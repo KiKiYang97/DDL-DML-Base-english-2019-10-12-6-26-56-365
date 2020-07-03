e23f5e7679fb751778997c9a60659970f49208e6
Dunka97 <1150311882@qq.com>
2020-07-03 19:26:08 +0800

    update ddl.sql、dml.sql

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,32 @@
 -- **Database level:**
 -- Display all the database
-
+show databases;
 -- Enter a certain database
-
+use database_name;
 -- Create a database
-
+create database database_name;
 -- Create the database of the designated character set
-
+create database database_name character set utf8;
 -- Display the creation information fo the database
-
+show create database database_name;
 -- Revise the codes of the database
-
+alter database database_name character set gb2312;
 -- Delete a database
-
+drop database database_name;
 -- **Table level**
 -- Revise table name
-
+alter table table_name rename new_table_name;
 -- Revise the field's data type
-
+alter table table_name modify field_name modify_type;
 -- Revise field name
-
+alter table table_name modify field_name new_field_name;
 -- Add field
-
+alter table table_name add field_name filed_type;
 -- Delete field
-
+alter table table_name drop field_name;
 -- Revise the table's storage engine
-
+alter table table_name engine=MyISAM;
 -- Delete the table's foreign key restriant
-
+alter table table_name drop foreign key foregin_key_name;
 -- Delete a table
+drop table table_name;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,12 @@
 -- Insert record
-
+insert into table_name (field1,field2,...fieldN) VALUES (value1,value2,...valueN);
+insert into student(id,name,age,sex) values('001','Colin',18,'male');
+insert into student(id,name,age,sex) values('002','Emma',20,'female');
 -- Revise record
-
+update table_name set field1 = value1,field2 = value2,...;
 --  Delete record
-
+delete from table_name where name='%lin';
 -- Search record
+select field1,...fieldN
+from table_name1,table_name2
+where [condition];
